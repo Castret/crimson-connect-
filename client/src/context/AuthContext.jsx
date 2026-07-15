6629c61b45d02984aae9ec8f8006812102cc31ae
Castret <allwinjeromeclage@gmail.com>
2026-07-15 15:38:05 +0530

    Replace localhost URLs

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -22,7 +22,7 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const token = localStorage.getItem('cc_token');
     if (user && token) {
-      const newSocket = io('http://localhost:5000', {
+      const newSocket = io(import.meta.env.VITE_API_URL,  {
         auth: { token }
       });
       setSocket(newSocket);
