0014f48630473f63155751f7b9748c1a6408957e
Castret <allwinjeromeclage@gmail.com>
2026-07-15 15:33:59 +0530

    Use Vercel environment variable for API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('cc_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
